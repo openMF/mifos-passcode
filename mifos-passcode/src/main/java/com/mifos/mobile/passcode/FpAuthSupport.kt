2c90db271e7ec1ee2547569064ba15a5029cd8a5
akashmeruva9 <akash.meruva2003@gmail.com>
2024-09-05 18:31:19 +0530

    Resolved Gradle Errors

--- mifos-passcode/src/main/java/com/mifos/mobile/passcode/FpAuthSupport.kt
+++ mifos-passcode/src/main/java/com/mifos/mobile/passcode/FpAuthSupport.kt
@@ -1,22 +1,26 @@
 package com.mifos.mobile.passcode
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
 import androidx.core.hardware.fingerprint.FingerprintManagerCompat
 
 object FpAuthSupport {
 
+    @SuppressLint("RestrictedApi")
     @JvmStatic
     fun checkAvailability(context: Context): Boolean {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
                 FingerprintManagerCompat.from(context).isHardwareDetected
     }
 
+    @SuppressLint("RestrictedApi")
     @JvmStatic
     fun isFingerprintRegistered(context: Context): Boolean {
         return FingerprintManagerCompat.from(context).hasEnrolledFingerprints()
     }
 
+    @SuppressLint("RestrictedApi")
     @JvmStatic
     fun checkAvailabiltyAndIfFingerprintRegistered(context: Context): Boolean {
         val fingerprintManagerCompat = FingerprintManagerCompat.from(context)
