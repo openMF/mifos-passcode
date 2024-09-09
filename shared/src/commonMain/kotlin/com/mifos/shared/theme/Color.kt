540772ac11ae902d909ce680d3b31c5266bdb1ed
Rajan Maurya <therajanmaurya@users.noreply.github.com>
2024-09-09 13:30:07 -0400

    Revert "Implemented Biometric Authentication in Compose Multi Platform"
    This reverts commit fa0d0a20684ec2dc59d57a70811659e6663031aa.

--- shared/src/commonMain/kotlin/com/mifos/shared/theme/Color.kt
+++
@@ -1,5 +0,0 @@
-package com.mifos.shared.theme
-
-import androidx.compose.ui.graphics.Color
-
-val blueTint = Color(0xFF03A9F4)
--- shared/src/commonMain/kotlin/com/mifos/shared/utility/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.mifos.shared.utility
-
-object Constants {
-    const val STEPS_COUNT = 2
-    const val PASSCODE_LENGTH = 4
-    const val VIBRATE_FEEDBACK_DURATION = 300L
-}
