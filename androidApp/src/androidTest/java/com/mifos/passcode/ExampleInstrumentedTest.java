7f7689d877cac586849908b9b308becdda530f31
Rajan Maurya <therajanmaurya@users.noreply.github.com>
2024-09-09 13:30:07 -0400

    Revert "Resolved Gradle Errors"
    This reverts commit 9484647f9fac3a78cc230f424b4d157b7f7a71a2.

--- androidApp/src/androidTest/java/com/mifos/passcode/ExampleInstrumentedTest.java
+++ androidApp/src/androidTest/java/com/mifos/passcode/ExampleInstrumentedTest.java
@@ -17,7 +17,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.mifos.passcode", appContext.getPackageName());
     }
